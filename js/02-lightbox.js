aa114f37f5b54f1502416203c7028d932164d4dc
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-16 20:11:58 +0300

    finish second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createItems(array) {
+  return array.reduce((akum, item) => {
+    return (
+      akum +
+      `<a class="gallery__item" href=${item.original}>
+        <img
+          class="gallery__image"
+          src=${item.preview}
+          alt=${item.description}
+        />
+      </a>`
+    );
+  }, '');
+}
+
+const galleryItemsHtml = createItems(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryItemsHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
