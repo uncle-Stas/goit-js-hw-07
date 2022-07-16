7b65eb41065cf036f22fd04c82e63655274fadd1
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-16 16:42:10 +0300

    added galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createItems(array) {
+  return array.reduce((akum, item) => {
+    return (
+      akum +
+      `<div class="gallery__item">
+        <a class="gallery__link" href=${item.original}>
+          <img
+            class="gallery__image"
+            src=${item.preview}
+            data-source=${item.original}
+            alt=${item.description}
+          />
+        </a>
+      </div>`
+    );
+  }, '');
+}
+
+const galleryItemsHtml = createItems(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryItemsHtml);
+
+gallery.addEventListener('click', showOriginalImage);
+
+function showOriginalImage(event) {
+  event.preventDefault();
+
+  const imageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src=${imageUrl}>`);
+
+  instance.show();
+}
